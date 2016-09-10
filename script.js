fc368533a4716af8e3525f11644e457db8ae56a6
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-09-10 15:36:24 -0500

    Use let in for loop

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-var names = ['Max', 'Adam', 'Nancy', 'Cedar'];
-
+// declaring variables with let
 function loadProfiles (userNames) {
 	if (userNames.length > 3) {
 		let loadingMessage = 'This may take awhile!';
 		// loadingMessage scoped to if block
 		console.log('loadingMessage: ', loadingMessage);
 		// let variables are not hoisted to top of function
-		// flashMessage trapped inside else block so will error out
+		// flashMessage trapped inside else block so will give reference error
 		console.log('flashMessage:', flashMessage);
 	} else {
 		let flashMessage = 'Loading!';
 	}
 }
 
-loadProfiles(names);
+loadProfiles(['Max', 'Adam', 'Nancy', 'Cedar']);
 
+// declarations with let in for loops
+function newFunction(userNames) {
+	for(let i in userNames) {
+			// outputs correct value for each iteration through loop
+			console.log('Fetched for ', userNames[i]);
+		}
+}
 
-// function loadProfiles(userNames) {
-// 	var i;
-// 	for(i in userNames) {
-// 			console.log('Fetched for ', userNames[i]);
-// 		}
-// 	// });
-// 	}
-// }
+newFunction(['Max', 'Adam', 'Nancy', 'Cedar']);
